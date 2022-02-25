1ede01ae005c87476fa627b46dd80926a8f725d4
mara-olson <mara.c.olson@gmail.com>
2022-02-24 21:07:32 -0800

    Made country_code required for international orders only, not domestic orders

--- melons.py
+++ melons.py
@@ -2,18 +2,25 @@
 
 class AbstractMelonOrder:
     """An abstract base class that other Melon Order inherit from."""
-    def __init__(self, species, qty, country_code):
+    def __init__(self, species, qty):
         """Initialize melon order attributes."""
 
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.country_code = country_code
+
 
     def get_total(self):
         """Calculate price, including tax."""
-
         base_price = 5
+
+        if self.species == 'Christmas melon':
+            base_price = base_price * 1.5
+
+        if self.country_code != 'USA':
+            if self.qty < 10:
+                base_price = base_price + 3
+        
         total = (1 + self.tax) * self.qty * base_price
 
         return total
@@ -35,6 +42,8 @@ class DomesticMelonOrder(AbstractMelonOrder):
     country_code = 'USA'
     order_type = 'domestic'
     tax = 0.08
+
+
    
 
 class InternationalMelonOrder(AbstractMelonOrder):
@@ -43,4 +52,17 @@ class InternationalMelonOrder(AbstractMelonOrder):
     order_type = "international"
     tax = 0.17
 
+    def __init__(self, species, qty, country_code):
+        super().__init__(species, qty)
+        self.country_code = country_code
 
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """Government orders."""
+    
+    tax = 0
+    passed_inspection = False
+
+    def mark_inspection(passed):
+        passed_inspection = True
+        return passed_inspection
