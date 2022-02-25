7d1edb932753001e849aa71e3f20afad20e4566b
mara-olson <mara.c.olson@gmail.com>
2022-02-24 20:30:35 -0800

    Refactored abstract class to hold methods for subclasses

--- melons.py
+++ melons.py
@@ -10,44 +10,6 @@ class AbstractMelonOrder:
         self.shipped = False
         self.country_code = country_code
 
-
-class DomesticMelonOrder(AbstractMelonOrder):
-    """A melon order within the USA."""
-    country_code = 'USA'
-    order_type = 'domestic'
-    tax = 0.08
-   
-
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
-
-
-class InternationalMelonOrder(AbstractMelonOrder):
-    """An international (non-US) melon order."""
-    
-    order_type = "international"
-    tax = 0.17
-
-    
-    #     """Initialize melon order attributes."""
-
-    #     self.species = species
-    #     self.qty = qty
-    #     self.country_code = country_code
-    #     self.shipped = False
-    #     self.order_type = "international"
-    #     self.tax = 0.17
-
     def get_total(self):
         """Calculate price, including tax."""
 
@@ -65,3 +27,20 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+    
+    country_code = 'USA'
+    order_type = 'domestic'
+    tax = 0.08
+   
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+    
+    order_type = "international"
+    tax = 0.17
+
+
