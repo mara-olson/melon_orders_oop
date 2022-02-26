1ae516cb9c84e36001fcf33d8f8bb79cfc496b85
mara-olson <mara.c.olson@gmail.com>
2022-02-26 13:03:52 -0800

    Added a class for Government orders to pass inspection, and added splurge pricing to the base class

--- melons.py
+++ melons.py
@@ -1,7 +1,10 @@
 """Classes for melon orders."""
 
+import random
+
 class AbstractMelonOrder:
     """An abstract base class that other Melon Order inherit from."""
+
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
 
@@ -9,16 +12,19 @@ class AbstractMelonOrder:
         self.qty = qty
         self.shipped = False
 
+    def get_base_price(self):
+        return random.randint(5, 10)
+
 
     def get_total(self):
         """Calculate price, including tax."""
-        base_price = 5
+
+        base_price = self.get_base_price()
 
         if self.species == 'Christmas melon':
             base_price = base_price * 1.5
 
-        if self.country_code != 'USA':
-            if self.qty < 10:
+        if self.country_code != 'USA' and self.qty < 10:
                 base_price = base_price + 3
         
         total = (1 + self.tax) * self.qty * base_price
@@ -34,6 +40,7 @@ class AbstractMelonOrder:
         """Return the country code."""
 
         return self.country_code
+        
 
 
 class DomesticMelonOrder(AbstractMelonOrder):
@@ -45,7 +52,6 @@ class DomesticMelonOrder(AbstractMelonOrder):
 
 
    
-
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
     
@@ -60,9 +66,16 @@ class InternationalMelonOrder(AbstractMelonOrder):
 class GovernmentMelonOrder(AbstractMelonOrder):
     """Government orders."""
     
+    country_code = 'USA'
+    order_type = 'domestic'
     tax = 0
-    passed_inspection = False
 
-    def mark_inspection(passed):
-        passed_inspection = True
-        return passed_inspection
+    def __init__(self, species, qty):
+        super().__init__(species, qty)
+        self.passed_inspection = False
+
+    def mark_inspection(self):
+        """Setting the inspection to passed status."""
+
+        self.passed_inspection = True
+        
